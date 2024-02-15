d40765c153fe0f299c097c523cec6d0341f73962
AJ1732 <ejemeniboi@gmail.com>
2024-02-15 16:43:14 +0100

    addeded components

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div className='font-poppins'>
+      <h1 className='
+        text-3xl font-bold
+        
+      '>Web Calculator</h1>      
+    </div>
   )
 }
 
