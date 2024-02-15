5ee4cfd511416782817d4d8408037e9f527fa0c1
AJ1732 <ejemeniboi@gmail.com>
2024-02-15 23:21:57 +0100

    feat: implemented num button

--- src/components/Buttons/NumButton.jsx
+++ src/components/Buttons/NumButton.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useTheme } from '../../Theme/ThemeProvider';
 
-const NumButton = () => {
+const NumButton = ({children}) => {
+  const { theme } = useTheme();
+  const themeBoolean = theme === 'light';
+
+  const handleClick = (e) => {
+    console.log(e.target.value, 'clicked!');
+  }
+
   return (
-    <div>NumButton</div>
+    <button 
+      className={` 
+        rounded-2xl text-2xl font-medium
+        ${themeBoolean? 'bg-lm-num-btn-bg text-lm-num-btn-text': 'bg-dm-num-btn-bg text-dm-num-btn-text'} `}
+      value={children}
+      onClick={handleClick}
+    >
+      {children}
+    </button>
   )
 }
 
--- src/components/Sections/Calc.jsx
+++ src/components/Sections/Calc.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useTheme } from '../../Theme/ThemeProvider';
+import NumButton from '../Buttons/NumButton';
 
 const Calc = () => {
   const { theme } = useTheme();
@@ -8,14 +9,46 @@ const Calc = () => {
   return (
     <main className={`
       max-w-96 max-h-[50.75rem] w-96 h-[50.75rem] 
-      m-auto | duration-300 ease-in-out | rounded-[2.5rem] 
-      ${ themeBoolean? 'bg-white': 'bg-blue-darker' }
+      grid grid-rows-[15rem_1fr] gap-5
+      m-auto p-8 | duration-300 ease-in-out | rounded-[2.5rem] 
+      ${ themeBoolean? 'bg-lm-bg': 'bg-dm-bg' }
     `}>
+      <div className='h-full bg-[#ccc] rounded-2xl'></div>
       
       <div className={`
-        
+        m-auto
+        grid grid-cols-[repeat(4,_60px)] grid-rows-[40px_repeat(5,_60px)] gap-5 place-content-center
       `}>
+        {/* FUNCTION BUTTONS */}
+        {['e', 'µ', 'sin', 'deg'].map(item => (
+          <div key={item} className='row-start-1 | bg-[#ccc] rounded-2xl'>{item}</div>
+        ))}
 
+        {/* NUMBER BUTTONS */}
+        {[1, 2, 3].map(item => (
+          <NumButton key={item} className='row-start-3'>{item}</NumButton>
+        ))}
+        {[4, 5, 6].map(item => (
+          <NumButton key={item} className='row-start-4 | bg-[#ccc] rounded-2xl'>{item}</NumButton>
+        ))}
+        {[7, 8, 9].map(item => (
+          <NumButton key={item} className='row-start-5 | bg-[#ccc] rounded-2xl'>{item}</NumButton>
+        ))}
+        <div className='row-start-6 col-start-1 col-span-2 | bg-[#ccc] rounded-2xl'>0</div>
+        
+        {/* ACTION BUTTONS */}
+        {['Ac', '←', '/'].map(item => (
+          <div key={item} className='row-start-2 | bg-[#ccc] rounded-2xl'>{item}</div>
+        ))}
+
+        {/* OPERATOR BUTTONS */}
+        <div className='row-start-2 | bg-[#ccc] rounded-2xl'>*</div>
+        <div className='row-start-6 col-start-3 | bg-[#ccc] rounded-2xl'>.</div>
+        <div className='row-start-3 col-start-4 | bg-[#ccc] rounded-2xl'>-</div>
+        <div className='grid grid-cols-subgrid gap-5 row-start-4 row-span-3 rounded-2xl |'>
+          <div className='row-start-1 row-span-1 | bg-[#ccc] rounded-2xl'>+</div>
+          <div className='row-start-2 row-span-1 | bg-[#ccc] rounded-2xl'>=</div>
+        </div>
       </div>
     </main>
   )
