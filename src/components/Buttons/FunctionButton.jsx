d497358ad8f0ec51d0507783acc0212f0a4112d3
AJ1732 <ejemeniboi@gmail.com>
2024-02-16 22:00:23 +0100

    feat: added function button component

---
+++ src/components/Buttons/FunctionButton.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useTheme } from '../../Theme/ThemeProvider';
+
+const FunctionButton = ({ children, className }) => {
+  const { theme } = useTheme();
+  const themeBoolean = theme === 'light';
+
+  const handleClick = (e) => {
+    console.log(e.target.value, 'clicked!');
+  }
+
+  return (
+    <button 
+      className={` 
+        ${className}
+        duration-300 ease-in-out
+        rounded-2xl text-2xl font-medium
+        ${themeBoolean? 'bg-lm-function-btn-bg text-lm-function-btn-text': 'bg-dm-function-btn-bg text-dm-function-btn-text'} `}
+      value={children}
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default FunctionButton
