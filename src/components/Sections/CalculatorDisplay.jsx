2c755a780d5667d38d492d039e69c3c0cb2bcc8b
AJ1732 <ejemeniboi@gmail.com>
2024-02-17 11:49:29 +0100

    feat: displayed the button value

--- src/components/Sections/CalculatorDisplay.jsx
+++ src/components/Sections/CalculatorDisplay.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { useTheme } from '../../Theme/ThemeProvider';
 import { useValueContext } from '../../Context/ContextProvider';
 
 const CalculatorDisplay = () => {
+  const { theme } = useTheme();
+  const themeBoolean = theme === 'light';
+
   const { value } = useValueContext();
-  console.log(value, 'clicked');
+  const numValue = Number(value)
+  console.log(typeof(numValue), numValue, 'clicked');
+  console.log(typeof(value), value, 'clicked');
 
   return (
-    <section className='h-full bg-[#ccc] rounded-2xl'>
-      <h2>{value}</h2>
+    <section className='
+      bg-[#ccc] h-full | p-10 rounded-2xl
+      grid place-items-end 
+    '>
+      <h2 className={`
+        font-medium text-5xl
+        ${themeBoolean? 'text-lm-answer-text': 'text-dm-answer-text'} 
+      `}>
+        {value}</h2>
     </section>
   )
 }
